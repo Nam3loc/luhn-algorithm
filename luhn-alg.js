7dbaa15e3e81507d93e8779bdbf716222e12b3ef
Coleman Escue <99232536+Nam3loc@users.noreply.github.com>
2022-12-15 17:49:18 -0500

    fixed some issues

--- luhn-alg.js
+++ luhn-alg.js
@@ -3,24 +3,29 @@
 function validCard(num) {
     const cardNumArr = [];
 
-    for(let i = num.length - 1; i > num; i--) {
+    for(let i = num.length - 1; i >= 0; i--) {
+        console.log('i am in the for loop')
         if(num[i] % 2 === 0) {
             cardNumArr.push(num[i]);
+            console.log('first log', cardNumArr);
         } else {
             num[i] *= 2;
             if(num[i] > 10) {
                 num[i] = (num[i] - 10) + 1;
                 cardNumArr.push(num[i]);
+                console.log('second log', cardNumArr);
             } else{
                 cardNumArr.push(num[i]);
+                console.log('third log', cardNumArr);
             }
-        } cardNumArr.reduce((partialSum, a) => partialSum + a, 0);
-        console.log(cardNumArr);
-        if(cardNumArr % 10 == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        } 
+    } cardNumArr.reduce((partialSum, a) => partialSum + a, 0);
+    console.log('im not going into the for loop');
+    console.log(cardNumArr);
+    if(cardNumArr % 10 == 0) {
+        return true;
+    } else {
+        return false;
     }
 }
 
